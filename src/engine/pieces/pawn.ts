57ce13933f2df02fe1b8ab66675d1b6ef9066e4b
Petdra75 <Petru.Draghiceanu@softwire.com>
2026-07-10 12:08:10 +0300

    added logic for generating pawn moves + made tests pass

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,13 +1,24 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+
+const WHITE_MOVE_DIRECTION = 1
+const BLACK_MOVE_DIRECTION = -1
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board) : Square[] {
+        const availablePawnMoves = new Array();
+        const moveDirection = this.player == Player.WHITE ? WHITE_MOVE_DIRECTION : BLACK_MOVE_DIRECTION
+        
+        const pawnSquare : Square = board.findPiece(this)
+        const forwardMove = new Square(pawnSquare.row + moveDirection, pawnSquare.col)
+        
+        availablePawnMoves.push(forwardMove)
+        return availablePawnMoves;
     }
 }
